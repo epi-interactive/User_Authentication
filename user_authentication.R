09d788fa27d903923147c31d1f6736115fe06f80
Epi-interactive <info@epi-interactive.com>
2021-08-18 08:59:07 +1200

    Updated file header

--- user_authentication.R
+++ user_authentication.R
@@ -1,11 +1,8 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+##################################
+# Created by EPI-interactive
+# 18 May 2019
+# https://www.epi-interactive.com
+##################################
 
 library(shiny)
 library(bcrypt)
